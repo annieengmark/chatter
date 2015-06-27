0a3517297f14f84a956fa572a5e777f6daa425b5
Annie Engmark <engmark.anne@gmail.com>
2015-06-27 16:31:50 -0500

    updates to project (core)

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -2,9 +2,12 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
             [hiccup.form :as form]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 (def chat-messages
   (atom '()))
@@ -14,7 +17,11 @@
   [messages]
   (page/html5
    [:head
-    [:title "chatter"]]
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "//chatter.css")]
+
    [:body
     [:h1 "Our Chat App"]
     [:p
@@ -24,13 +31,21 @@
       "Message: " (form/text-field "msg")
       (form/submit-button "Submit"))]
     [:p
-     [:table
+     [:table#messages.table.table-hover
       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 
 (defn update-messages!
   "This will update a message list atom"
   [messages name new-message]
   (swap! messages conj {:name name :message new-message}))
+
+(defn init []
+  (println "chatter is starting"))
+
+(defn destroy []
+  (println "chatter is shutting down"))
+
+
 
 (defroutes app-routes
   (GET "/" [] (generate-message-view @chat-messages))
@@ -40,6 +55,11 @@
               new-messages (update-messages! chat-messages name-param msg-param)]
           (generate-message-view new-messages)
           ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
